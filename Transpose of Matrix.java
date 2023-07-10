03992d59743cbd4da3962c66c700c71e23fb59ce
Sai Krishna <121814056+krishna-o73@users.noreply.github.com>
2023-07-10 23:56:20 +0530

    Create Transpose of Matrix.java

---
+++ Transpose of Matrix.java
@@ -0,0 +1,16 @@
+//https://practice.geeksforgeeks.org/problems/transpose-of-matrix-1587115621/1
+class Solution
+{
+    public void transpose(int n,int a[][])
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+        }
+    }
+}
