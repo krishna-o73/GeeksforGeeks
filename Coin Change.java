c6dd4731c03e67242bceacc044c1fc8b023b4742
Sai Krishna <121814056+krishna-o73@users.noreply.github.com>
2023-08-11 21:40:17 +0530

    Create Coin Change.java

---
+++ Coin Change.java
@@ -0,0 +1,25 @@
+//https://practice.geeksforgeeks.org/problems/coin-change2448/1
+class Solution {
+    public long count(int coins[], int N, int sum) {
+        // code here.
+        
+        
+        long[][] dp = new long[N + 1][sum + 1];
+
+    for (int i = 0; i <= N; i++) {
+        dp[i][0] = 1;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (coins[i - 1] <= j) {
+                dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[N][sum];
+    }
+}
